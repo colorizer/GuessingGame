b22ad8c6d2ba29b871a1cdddb07af4849b5d1893
colorizer <jebin12raj@gmail.com>
2020-05-06 17:27:33 +0530

    Added Error handling for non-integers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # The Guessing Game
 ### Status: 
-1. Date and Time of Run: **Tuesday 05 May 2020 06:06:16 PM IST**
-2. Number of lines in *guessinggame.sh*: **23**
+1. Date and Time of Run: **Wednesday 06 May 2020 05:20:26 PM IST**
+2. Number of lines in *guessinggame.sh*: **28**
--- guessinggame.sh
+++ guessinggame.sh
@@ -8,12 +8,17 @@ local response=''
 
 echo "How many files are here? Take a guess..."
 
-while [[ $response -ne $file_count ]]; do
+while [[ $((10#$response)) != $file_count ]]; do
 	read response
-	if [[ $response -gt $file_count ]]; then
+	if [[ $response =~ ^[0-9]+$ ]]; then
+	if [[ $((10#$response)) -gt $file_count ]]; then
 		echo "Nope.That guess was too high. Take another shot..."
-	elif [[ $response -lt $file_count ]]; then
+	elif [[ $((10#$response)) -lt $file_count ]]; then
 		echo "Nope.That guess was too low. Take another shot..."
+	fi
+	else
+		echo "I don't understand. Speak in numbers I know"
+		response=''
 	fi
 done
 
